572bc80ac7949cc370e9977bb6eca307f570b0a7
nanderss <nmander@outlook.com>
2018-09-16 15:30:30 -0400

    new

---
+++ app/src/main/java/com/example/niklas/efc_master/protocol.java
@@ -0,0 +1,20 @@
+package com.example.niklas.efc_master;
+
+public class protocol
+{
+    public static byte PHONE_PROTOCOL_NBR = 3;
+    public static byte PHONE_PROTOCOL_BYTES = 4;
+    public static byte BTN_TOOL_SELECT = 1;
+    public static byte BTN_TRIM_MODE = 2;
+    public static byte BTN_STOP = 3;
+    public static byte STOP_OFF = 0;
+    public static byte STOP_ON = 1;
+    public static byte NORMAL_TRIM = 0;
+    public static byte LITE_TRIM = 1;
+    public static byte TOOL_STRING = 0;
+    public static byte TOOL_BLADE = 1;
+    public static byte TOOL_EDGER = 2;
+    public static byte TOOL_TILLER = 3;
+    public static byte TOOL_BLOWER = 4;
+    public static byte TOOL_POLE_SAW = 5;
+}
