9557f04607249ada5fce640c57bcd991db18cfc9
nanderss <nmander@outlook.com>
2018-10-11 21:54:10 -0400

    Last run_time coming from BLE module

---
+++ app/src/main/java/com/example/niklas/efc_master/profiles/myLastRunYvalueFormatter.java
@@ -0,0 +1,22 @@
+package com.example.niklas.efc_master.profiles;
+
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.DecimalFormat;
+
+public class myLastRunYvalueFormatter implements IValueFormatter {
+
+    private DecimalFormat mFormat;
+
+    public myLastRunYvalueFormatter() {
+        mFormat = new DecimalFormat("###.#"); // use no decimals
+    }
+
+    @Override
+    public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+        // write your logic here
+        return mFormat.format(value); // e.g. append a dollar-sign
+    }
+}
